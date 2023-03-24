b14a10b04012201ae01e86f2d7e6c833b5aef3f4
Dragon <dragon.huang@chelpis.com>
2023-03-24 20:23:28 +0800

    repo(crawler): init crawler

---
+++ packages/crawler/.eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  root: true,
+  extends: ['custom'],
+  env: {
+    node: true,
+    es6: true,
+  },
+  parserOptions: {
+    project: ['tsconfig.json', 'tsconfig.dev.json'],
+    tsconfigRootDir: __dirname,
+    ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
+    sourceType: 'module', // Allows for the use of imports
+  },
+  ignorePatterns: ['build/**'],
+};
